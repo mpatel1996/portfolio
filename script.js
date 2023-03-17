49575bc37a402b0802e51c940f27414716228db7
ToFlux <mpjv12343@gmail.com>
2023-03-17 15:21:55 -0500

    format update

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 let theme = localStorage.getItem("theme");
 
 if (theme == null) {
-  setTheme("light");
+	setTheme("light");
 } else {
-  setTheme(theme);
+	setTheme(theme);
 }
 
 let themeDots = document.getElementsByClassName("theme-dot");
 
 for (var i = 0; themeDots.length > i; i++) {
-  themeDots[i].addEventListener("click", function () {
-    let mode = this.dataset.mode;
-    //  console.log("Option clicked:", mode);
-    setTheme(mode);
-  });
+	themeDots[i].addEventListener("click", function () {
+		let mode = this.dataset.mode;
+		//  console.log("Option clicked:", mode);
+		setTheme(mode);
+	});
 }
 
 function setTheme(mode) {
-  if (mode == "light") {
-    document.getElementById("theme-style").href = "default.css";
-  }
+	if (mode == "light") {
+		document.getElementById("theme-style").href = "default.css";
+	}
 
-  if (mode == "dark") {
-    document.getElementById("theme-style").href = "dark.css";
-  }
+	if (mode == "dark") {
+		document.getElementById("theme-style").href = "dark.css";
+	}
 
-  localStorage.setItem("theme", mode);
+	localStorage.setItem("theme", mode);
 }
--- tooltip.js
+++ tooltip.js
@@ -1,16 +1,15 @@
 var tooltipTimeout;
 
 $("#hover-tt").hover(function () {
-      tooltipTimeout = setTimeout(showTooltip, 1500);
-   },
-   hideTooltip);
+	tooltipTimeout = setTimeout(showTooltip, 1500);
+}, hideTooltip);
 
 function showTooltip() {
-   var tooltip = $("<div id='tooltip' class='tooltip'>I'm the tooltip!</div>");
-   tooltip.appendTo($("#hover-tt"));
+	var tooltip = $("<div id='tooltip' class='tooltip'>I'm the tooltip!</div>");
+	tooltip.appendTo($("#hover-tt"));
 }
 
 function hideTooltip() {
-   clearTimeout(tooltipTimeout);
-   $("#tooltip").fadeOut().remove();
+	clearTimeout(tooltipTimeout);
+	$("#tooltip").fadeOut().remove();
 }
